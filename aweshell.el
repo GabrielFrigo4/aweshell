ba40a40532c372157dc7813b6e79d560da85c9df
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2025-04-18 16:56:30 -0300

    Update Highlight

--- aweshell.el
+++ aweshell.el
@@ -827,7 +827,7 @@ This advice can make `other-window' skip `aweshell' dedicated window."
   "Idle timer for highlight eshell command.")
 (make-variable-buffer-local 'aweshell--highlight-timer)
 
-(defvar aweshell-highlight-delay (expt 2 -4)
+(defvar aweshell-highlight-delay (expt 2 -6)
   "Idle timer delay for highlight eshell command.")
 
 (defun aweshell-start-highlight-timer ()
